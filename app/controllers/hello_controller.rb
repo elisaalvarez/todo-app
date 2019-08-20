da941022fae3b765b565b5d35b2793593ebcda66
sp4ceunicorn <colorful_elisa@hotmail.com>
2019-08-20 12:59:04 -0500

    New login and logout design and task views creation

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,5 +1,4 @@
 class HelloController < ApplicationController
-  before_action :authenticate_user!
   def index
     @uwu = 'Hello'
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :tasks
   resources :lists
   get 'hello/index'
   # get 'devise/sessions#destroy'
   devise_for :users
-  root :to => "lists#index"
+  root :to => "hello#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
